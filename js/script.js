f792199a6ef0847778884bbd8646d09a503bd7d5
Sarthak Upadhyay <sarthak7u@gmail.com>
2016-03-05 15:35:08 +0530

    Update Gallery Images

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 CURR_IMAGE = 0;
 NUM_IMAGES = 7;
 IMAGES = ['2.jpeg', '3.jpeg', '4.jpeg', '5.jpeg', '6.jpeg', '7.jpeg', '1.jpeg'];
+// COLORS = ['#311b92', '#b71c1c', '#6574A9', '#6d4c41']
+COLORS = ['rgb(34, 119, 119)', 'rgb(82, 51, 110)', 'rgb(109, 64, 51)', 'rgb(87, 125, 58)', 'rgba(101, 116, 169, 1)'];
 function slideShow()
 {
-    setInterval(changeImage, 3000)
+    setInterval(changeImage, 4000)
 }
 function changeImage()
 {
@@ -11,15 +13,18 @@ function changeImage()
     $('.background-image-home').css('background', "url('img/"+ IMAGES[CURR_IMAGE % 7] +"')");
     $('.background-image-home').css('background-position', 'center');
     $('.background-image-home').css('background-size', 'cover');
+    $('.nav-top').css('background', COLORS[CURR_IMAGE%5]);
+    $('.navbar-default').css('background', COLORS[CURR_IMAGE%5]);
 
     CURR_IMAGE += 1;
 }
 function addImages()
 {
     var divs = $('.gallery-wrapper').children();
-    for (var i = 0; i < 16; i++)
+    for (var i = 0; i < 44; i++)
     {
-        $(divs[i]).css('background', "url('img/"+ IMAGES[(i+1) % 5] +"')");
+        var j = i+ 1;
+        $(divs[i]).css('background', "url('img/gallery/img ("+j+").jpg')");
         $(divs[i]).css('background-size', 'cover');
     }
 }
